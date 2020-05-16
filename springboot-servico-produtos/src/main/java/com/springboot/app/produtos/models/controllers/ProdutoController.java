a266040468880b7fa65ac2294aa6b6709e4ec942
Fernando Jose <openline_desenvolvimento2@outlook.com>
2020-05-16 10:58:51 -0300

    Configuração de timeout do hystrix

--- springboot-servico-produtos/src/main/java/com/springboot/app/produtos/models/controllers/ProdutoController.java
+++ springboot-servico-produtos/src/main/java/com/springboot/app/produtos/models/controllers/ProdutoController.java
@@ -9,6 +9,8 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.springboot.app.produtos.models.entity.Produto;
 import com.springboot.app.produtos.models.service.IProdutoService;
 
@@ -20,14 +22,14 @@ public class ProdutoController {
 
 	@Value("${server.port}")
 	private Integer port;
-	
+
 	@Autowired
 	private IProdutoService produtoService;
 
 	@GetMapping("/listar")
 	public List<Produto> listar() {
 		return produtoService.findAll().stream().map(produto -> {
-			//produto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			// produto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 			produto.setPort(port);
 			return produto;
 		}).collect(Collectors.toList());
@@ -37,8 +39,17 @@ public class ProdutoController {
 	public Produto getProduto(@PathVariable Long id) {
 		Produto produto = produtoService.findByid(id);
 		produto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
-		
+
+		// Para teste da configuração timeout do @HystrixCommand
+		// try {
+		// Thread.sleep(8000L);
+		// } catch (InterruptedException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+
 		return produto;
+
 	}
 
 }
