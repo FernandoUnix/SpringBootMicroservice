6b428341147dcf69a1ec8fc83a2a405a2740f387
Fernando Jose <openline_desenvolvimento2@outlook.com>
2020-05-16 09:57:19 -0300

    Adicionado eureka server

---
+++ springboot-servico-eureka-server/src/test/java/com/springbootservicoeureka/SpringbootServicoEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootservicoeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicoEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
